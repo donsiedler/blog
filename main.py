6a5bb944eb3f7c57b3d7cefa949acf4806c00cfa
Artur Siedlerski <don.siedler@gmail.com>
2023-03-22 20:43:58 +0100

    Add contact route

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def main():
     return render_template("index.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run()
