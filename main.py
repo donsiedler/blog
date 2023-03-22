2f2579eb2caa6ef5b35746b49508e80d7b6edef7
Artur Siedlerski <don.siedler@gmail.com>
2023-03-22 21:41:18 +0100

    Get fake posts from API and pass them as context

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+FAKE_POSTS_URL = "https://api.npoint.io/c790b4d5cab58020d391"
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    response = requests.get(FAKE_POSTS_URL)
+    response.raise_for_status()
+    posts = response.json()
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/contact")
